717cc2de95b872bcdad3564e725418d735dc31df
Sitthinan-Chaiyasit <darksideez62@gmail.com>
2025-09-24 18:36:33 +0700

    fix vercel again

--- worklog/wsgi.py
+++ worklog/wsgi.py
@@ -13,5 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worklog.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+# สำหรับ Vercel serverless ให้ alias
+app = application
 
